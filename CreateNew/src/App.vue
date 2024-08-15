9f8d81148021abb2e599fff7b51392640a84f8ee
Babu Nair <babunairofficial@gmail.com>
2024-08-15 18:38:21 +0530

    Vue components

--- CreateNew/src/App.vue
+++ CreateNew/src/App.vue
@@ -10,10 +10,21 @@ export default{
 </script>
 
 <template>
-  <h1>{{message}}</h1>
+  <h1>Food</h1>
+  <food-item/>
+  <food-item/>
+  <food-item/>
   
 </template>
 
 <style>
+#app>div{
+  border: dashed black 1px;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightgreen;
+  cursor: pointer;
+}
 
 </style>
---
+++ CreateNew/src/components/FoodItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-on:click="counter">
+        <h2>{{ name }}</h2>
+        <p>{{ message }}</p>
+        <p id="red">This item has been clicked {{ ctr }} times.</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        data(){
+            return {
+                name: 'Apples',
+                message: 'I like apples',
+                ctr:0
+            }
+        },
+        methods:{
+            counter(){
+                this.ctr++;
+            }
+        }
+    }
+</script>
+
+<style>
+    #red{
+        font-weight:bold;
+        color: red;
+    }
+</style>
